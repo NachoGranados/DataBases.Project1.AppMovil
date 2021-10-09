1648a59e359a1e3843b4a4c84177ea04284bb163
Ignacio Granados <ignaciogramar@gmail.com>
2021-10-09 08:45:58 -0600

    Adding register window

--- DataBasesProject1/app/src/main/java/com/example/cinetec/LoginActivity.java
+++ DataBasesProject1/app/src/main/java/com/example/cinetec/LoginActivity.java
@@ -2,13 +2,52 @@ package com.example.cinetec;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        registerButton = (Button) findViewById(R.id.buttonLoginRegister);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                openRegisterActivity();
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
+    private void openRegisterActivity() {
+
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+
+    }
+
 }
